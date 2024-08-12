4261a9fe93cf83db59867ee1c0fbf5908904c082
leezh0701 <leezh0701@gmail.com>
2024-08-12 13:30:42 +1000

    Initial commit

---
+++ fit5032-week1/src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Github URL</h1>
+    <a href="https://github.com/leezh0701" target="_blank">https://github.com/leezh0701</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Github URL'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+
+a {
+  color: green;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,18 @@
+<!-- src/components/MyName.vue -->
+<template>
+  <div>
+    <h2>Welcome!Zhehao!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+</style>
